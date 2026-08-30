5b7a4bc19837317b0dd3e19f3b9e6353ab92a74a
Fernando Nieto Morales <fernando.nietoms@gmail.com>
2026-08-30 00:35:36 -0600

    Mono

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,25 +5,31 @@ public class PlayerController : MonoBehaviour
     [SerializeField] Rigidbody2D rigidbody;
     [SerializeField] float maxSpeed = 0.1f;
     [SerializeField] float speed = 0.01f;
+    [SerializeField] Animator animator;
+    int speedParam;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        speedParam = Animator.StringToHash("Speed");
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool isMoving = false;
         if (Input.GetKey(KeyCode.A))
         {
-            //this.transform.position += Vector3.left * Time.deltaTime * cameraSpeed;
             rigidbody.AddForceX(-speed, ForceMode2D.Impulse);
+            isMoving = true;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            //this.transform.position += Vector3.right * Time.deltaTime * cameraSpeed;
             rigidbody.AddForceX(speed, ForceMode2D.Impulse);
+            isMoving = true;
         }
+
+        UpdateMove(isMoving);
 
         if (rigidbody.linearVelocity.x > maxSpeed) rigidbody.linearVelocity = new Vector2(maxSpeed, 0);
         if (rigidbody.linearVelocity.x < -maxSpeed) rigidbody.linearVelocity = new Vector2(-maxSpeed, 0);
@@ -36,5 +42,17 @@ public class PlayerController : MonoBehaviour
         {
             rigidbody.AddForceX(-speed, ForceMode2D.Impulse);
         }
+    }
+
+    void UpdateMove(bool isMoving)
+    {
+        float movement = animator.GetFloat(speedParam);
+        if (isMoving) movement += Time.deltaTime;
+        else movement -= Time.deltaTime * 10;
+
+        if (movement < 0) movement = 0;
+        if (movement > 1) movement = 1;
+
+        animator.SetFloat(speedParam, movement);
     }
 }
