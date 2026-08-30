5ea9717d6cc18682c5e6b2a382533538ee5152b2
Fernando Nieto Morales <fernando.nietoms@gmail.com>
2026-08-30 00:05:26 -0600

    Texto

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,8 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
@@ -17,6 +19,18 @@ public class GameController : MonoBehaviour
     [SerializeField]
     GameObject replayButton;
 
+    [SerializeField]
+    GameObject comic;
+    [SerializeField]
+    Image comicImage;
+    [SerializeField]
+    Image comicEmpty;
+    [SerializeField]
+    Image comicOne;
+    [SerializeField]
+    Image comicTwo;
+    Coroutine comicRoutine;
+
     private void Awake()
     {
         Instance = this;
@@ -26,6 +40,19 @@ public class GameController : MonoBehaviour
     {
         playerController.enabled = false;
         monster.enabled = false;
+        comicRoutine = StartCoroutine(PlayComic());
+    }
+
+    private void Update()
+    {
+        if (comicRoutine != null)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                StopCoroutine(comicRoutine);
+                comic.SetActive(false);
+            }
+        }
     }
 
     public void StartGame()
@@ -46,5 +73,48 @@ public class GameController : MonoBehaviour
     public void ReloadGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    IEnumerator PlayComic()
+    {
+        comic.SetActive(true);
+        comicImage.color = new Color(comicImage.color.r, comicImage.color.g, comicImage.color.b, 1);
+        comicEmpty.color = new Color(comicEmpty.color.r, comicEmpty.color.g, comicEmpty.color.b, 0);
+        comicOne.color = new Color(comicOne.color.r, comicOne.color.g, comicOne.color.b, 0);
+        comicTwo.color = new Color(comicTwo.color.r, comicTwo.color.g, comicTwo.color.b, 0);
+
+        while (comicEmpty.color.a < 1)
+        {
+            comicEmpty.color += new Color(0, 0, 0, Time.deltaTime);
+            yield return null;
+        }
+
+        comicImage.color = new Color(comicImage.color.r, comicImage.color.g, comicImage.color.b, 0);
+
+        while (comicOne.color.a < 1)
+        {
+            comicOne.color += new Color(0, 0, 0, Time.deltaTime);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(3);
+
+        while (comicTwo.color.a < 1)
+        {
+            comicTwo.color += new Color(0, 0, 0, Time.deltaTime);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(2);
+
+
+        while (comicEmpty.color.a > 0)
+        {
+            comicEmpty.color -= new Color(0, 0, 0, Time.deltaTime);
+            comicOne.color -= new Color(0, 0, 0, Time.deltaTime);
+            comicTwo.color -= new Color(0, 0, 0, Time.deltaTime);
+            yield return null;
+        }
+        comic.SetActive(false);
     }
 }
