0fb50e7499ac4a2b806b136aba09b237fac5e50a
FrankerPool <39344039+FrankerPool@users.noreply.github.com>
2026-08-30 02:08:28 -0600

    Fix: player PS

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,57 +2,71 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] Rigidbody2D rigidbody;
-    [SerializeField] float maxSpeed = 0.1f;
-    [SerializeField] float speed = 0.01f;
-    [SerializeField] Animator animator;
-    int speedParam;
+	// Cambié el nombre a 'rb' ya que 'rigidbody' puede causar advertencias por ocultar propiedades heredadas
+	[SerializeField] Rigidbody2D rb;
+	[SerializeField] float maxSpeed = 5f;
+	[SerializeField] float speed = 10f; // Necesitarás valores más altos si usas ForceMode2D.Force
+	[SerializeField] Animator animator;
+	int speedParam;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        speedParam = Animator.StringToHash("Speed");
-    }
+	private float moveInput = 0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        bool isMoving = false;
-        if (Input.GetKey(KeyCode.A))
-        {
-            rigidbody.AddForceX(-speed, ForceMode2D.Impulse);
-            isMoving = true;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            rigidbody.AddForceX(speed, ForceMode2D.Impulse);
-            isMoving = true;
-        }
+	void Start()
+	{
+		speedParam = Animator.StringToHash("Speed");
+	}
 
-        UpdateMove(isMoving);
+	void Update()
+	{
+		// 1. Recoger el input en Update (dependiente del framerate)
+		moveInput = 0f;
+		if (Input.GetKey(KeyCode.A)) moveInput = -1f;
+		if (Input.GetKey(KeyCode.D)) moveInput = 1f;
 
-        if (rigidbody.linearVelocity.x > maxSpeed) rigidbody.linearVelocity = new Vector2(maxSpeed, 0);
-        if (rigidbody.linearVelocity.x < -maxSpeed) rigidbody.linearVelocity = new Vector2(-maxSpeed, 0);
+		bool isMoving = moveInput != 0f;
+		UpdateMove(isMoving);
+	}
 
-        if (this.transform.position.x < -15.0f)
-        {
-            rigidbody.AddForceX(speed, ForceMode2D.Impulse);
-        }
-        if (this.transform.position.x > 65.0f)
-        {
-            rigidbody.AddForceX(-speed, ForceMode2D.Impulse);
-        }
-    }
+	void FixedUpdate()
+	{
+		// 2. Aplicar físicas en FixedUpdate (independiente del framerate)
+		if (moveInput != 0f)
+		{
+			// Usamos 'Force' para movimiento continuo al mantener la tecla presionada
+			rb.AddForceX(moveInput * speed, ForceMode2D.Force);
+		}
 
-    void UpdateMove(bool isMoving)
-    {
-        float movement = animator.GetFloat(speedParam);
-        if (isMoving) movement += Time.deltaTime;
-        else movement -= Time.deltaTime * 10;
+		// 3. Limitar velocidad manteniendo la velocidad Y intacta (para no romper la gravedad)
+		float currentVelX = rb.linearVelocity.x;
+		if (currentVelX > maxSpeed)
+		{
+			rb.linearVelocity = new Vector2(maxSpeed, rb.linearVelocity.y);
+		}
+		else if (currentVelX < -maxSpeed)
+		{
+			rb.linearVelocity = new Vector2(-maxSpeed, rb.linearVelocity.y);
+		}
 
-        if (movement < 0) movement = 0;
-        if (movement > 1) movement = 1;
+		// 4. Límites del mapa (Aquí sí tiene sentido el Impulse como empuje único)
+		if (this.transform.position.x < -15.0f)
+		{
+			rb.AddForceX(speed, ForceMode2D.Impulse);
+		}
+		else if (this.transform.position.x > 65.0f)
+		{
+			rb.AddForceX(-speed, ForceMode2D.Impulse);
+		}
+	}
 
-        animator.SetFloat(speedParam, movement);
-    }
+	void UpdateMove(bool isMoving)
+	{
+		float movement = animator.GetFloat(speedParam);
+		if (isMoving) movement += Time.deltaTime;
+		else movement -= Time.deltaTime * 10;
+
+		// Clamp01 es una forma más limpia de mantener el valor entre 0 y 1
+		movement = Mathf.Clamp01(movement);
+
+		animator.SetFloat(speedParam, movement);
+	}
 }
